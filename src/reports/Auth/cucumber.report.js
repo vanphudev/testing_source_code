977c24130b86a3fa031003bc6436a9c58291ba86
nhut201425 <nmn20082003@gmail.com>
2024-10-22 00:05:19 +0700

    Nhut

--- src/reports/Auth/cucumber.report.js
+++ src/reports/Auth/cucumber.report.js
@@ -1,10 +1,10 @@
 const reporter = require("cucumber-html-reporter");
 
 const options = {
-   jsonFile: "src/reports/cucumber-report.json",
+   jsonFile: "src/reports/Auth/cucumber-report.json",
    launchReport: true,
    name: "Cucumber Report - Login Feature - Tester Nguyễn Văn Phú",
-   output: "src/reports/cucumber-report-reporter.html",
+   output: "src/reports/Auth/cucumber-report-reporter.html",
    theme: "bootstrap",
 };
 
